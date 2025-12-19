dd8037828d1112c6d7e8023cc6696135a3a053c4
AndanaSat <andana.s.her@gmail.com>
2025-12-19 16:06:07 +0700

    Updated validateForm

--- data/assets/js/validate.js
+++ data/assets/js/validate.js
@@ -1,55 +1,48 @@
 const validateForm = (form) => {
     form.setAttribute('novalidate', '');
-
     const rows = form.querySelectorAll('.row');
 
     const validateRow = (row) => {
         const input = row.querySelector('input');
         const errorMsg = row.querySelector('.note, #error-message');
-        let rowValid = true;
+        errorMsg.textContent = '';
 
         if (!input) {
             return true;
         }
 
-        if (errorMsg) {
-            errorMsg.textContent = '';
-        }
-
         if (input.hasAttribute('required') && input.value.trim() === '') {
             if (errorMsg) {
                 errorMsg.textContent = 'This field is required.';
-                rowValid = false;
+                return false;
             }
         }
-        if (rowValid && input.hasAttribute('minlength')) {
+        if (input.hasAttribute('minlength')) {
             const min = parseInt(input.getAttribute('minlength'))
             if (input.value.length < min) {
                 errorMsg.textContent = errorMsg.textContent = `Minimum length is ${min} characters.`;
-                rowValid = false;
+                return false;
             }
         }
 
-        return rowValid;
+        return true;
     }
 
     let allValid = true;
 
     rows.forEach(row => {
+        // initial validation
         if (!validateRow(row)) {
             allValid = false;
         }
 
+        // re-validate after input change
         const input = row.querySelector("input");
-
-        if (!input) {
-            return;
-        }
 
         input.addEventListener('input', () => {
             validateRow(row);
         });
-    });
+    })
 
     return allValid;
 }
